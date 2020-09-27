ce43ee51aa63cb4da4150ae2147358f5205bc390
liukun <919590347@qq.com>
2020-09-27 23:06:50 +0800

    feat: 增加@react/types

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import './index.less'
 
+interface ButtonProps {
+    label: string
+}
+
+const Button: React.FC<ButtonProps> = ({ label }) => {
+    return (
+        <button>
+            { label }
+        </button>
+    ) 
+}
+
 ReactDom.render(
-    <div className="container">hot render</div>,
+    <div className="container">
+        <Button label="hot render"/>
+    </div>,
     document.getElementById('root')
 )

5a5870391413f3fb8fe2ebe9a34f600b777cf52a
liukun <919590347@qq.com>
2020-12-03 22:10:40 +0800

    feat: css module自动转换camelCase

--- config/webpack.base.ts
+++ config/webpack.base.ts
@@ -41,6 +41,7 @@ const baseConfig = () => {
                 importLoaders: 1,
                 modules: {
                     localIdentName: '[name]__[local]--[hash:base64:5]',
+                    exportLocalsConvention: "camelCase",
                     auto: (resourcePath) => /.module.(css|less)$/i.test(resourcePath),
                 },
             },
